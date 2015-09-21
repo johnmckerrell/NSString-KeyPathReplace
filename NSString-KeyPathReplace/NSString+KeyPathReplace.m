fc050bd7e7f666a90d2387213319e613d71860c6
John McKerrell <john@mckerrell.net>
2015-09-22 00:57:15 +0100

    More tests and fixing an issue when key path call causes an exception.

--- NSString-KeyPathReplace/NSString+KeyPathReplace.m
+++ NSString-KeyPathReplace/NSString+KeyPathReplace.m
@@ -17,34 +17,38 @@
 }
 
 - (NSString*)stringByReplacingKeyPathBindingsWithDictionaryValues:(NSDictionary*)dict defaultValues:(NSDictionary*)defaultValues {
-    return [self stringByReplacingOccurrencesOfRegex:@"\\\\?\\{([^}:]+)(:([^}]+))?\\}"
-                                          usingBlock:^NSString *(NSInteger captureCount, NSString *const __unsafe_unretained *capturedStrings, const NSRange *capturedRanges, volatile BOOL *const stop) {
-                                              NSString *keyPath = nil;
-                                              NSString *defaultValue = nil;
-                                              if (captureCount>1) {
-                                                  keyPath = capturedStrings[1];
-                                              }
-                                              if (captureCount>3 && [capturedStrings[3] length]) {
-                                                  defaultValue = capturedStrings[3];
-                                              }
-                                              if (![defaultValue length] && defaultValues && [defaultValues[keyPath] length]) {
-                                                  defaultValue = defaultValues[keyPath];
-                                              }
-                                              NSString *val = @"";
-                                              if ([capturedStrings[0] characterAtIndex:0] == '\\') {
-                                                  val = [capturedStrings[0] substringFromIndex:1];
-                                              } else if (keyPath) {
-                                                  val = [dict valueForKeyPath:keyPath];
-                                                  if (val == (id)[NSNull null]) {
-                                                      if (defaultValue) {
-                                                          val = defaultValue;
-                                                      } else {
-                                                          val = @"";
-                                                      }
-                                                  }
-                                              }
-                                              return val;
-                                          }];
+    return [self stringByReplacingOccurrencesOfRegex:@"\\\\?\\{([^}:]+)(:([^}]+))?\\}" usingBlock:^NSString *(NSInteger captureCount, NSString *const __unsafe_unretained *capturedStrings, const NSRange *capturedRanges, volatile BOOL *const stop) {
+        NSString *keyPath = nil;
+        NSString *defaultValue = nil;
+        if (captureCount>1) {
+            keyPath = capturedStrings[1];
+        }
+        if (captureCount>3 && [capturedStrings[3] length]) {
+            defaultValue = capturedStrings[3];
+        }
+        if (![defaultValue length] && defaultValues && [defaultValues[keyPath] length]) {
+            defaultValue = defaultValues[keyPath];
+        }
+        NSString *val = @"";
+        if ([capturedStrings[0] characterAtIndex:0] == '\\') {
+            val = [capturedStrings[0] substringFromIndex:1];
+        } else if (keyPath) {
+            @try {
+                val = [dict valueForKeyPath:keyPath];
+            }
+            @catch (NSException *exception) {
+                val = nil;
+            }
+            if (val == (id)[NSNull null]) {
+                if (defaultValue) {
+                    val = defaultValue;
+                } else {
+                    val = @"";
+                }
+            }
+        }
+        return val;
+    }];
     
 }
 
--- NSString-KeyPathReplaceTests/NSString_KeyPathReplaceTests.m
+++ NSString-KeyPathReplaceTests/NSString_KeyPathReplaceTests.m
@@ -8,6 +8,8 @@
 
 #import <XCTest/XCTest.h>
 
+#import "NSString+KeyPathReplace.h"
+
 @interface NSString_KeyPathReplaceTests : XCTestCase
 
 @end
@@ -24,15 +26,84 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testVeryBasic {
+    NSDictionary *dict = @{
+                           @"value": @"World"
+                           };
+    NSString *result = [@"Hello {value}!" stringByReplacingKeyPathBindingsWithDictionaryValues:dict
+                                                                                defaultValues:nil];
+    XCTAssertEqualObjects(result, @"Hello World!");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
+- (void)testEscaping {
+    NSDictionary *dict = @{
+                           @"value": @"Vulcan"
+                           };
+    NSString *result = [@"Hello \\{value}!" stringByReplacingKeyPathBindingsWithDictionaryValues:dict
+                                                                                 defaultValues:nil];
+    XCTAssertEqualObjects(result, @"Hello {value}!");
+}
+
+- (void)testTwoLevels {
+    NSDictionary *dict = @{
+                           @"planet": @"World",
+                           @"person": @{
+                                   @"firstname": @"Joe",
+                                   @"lastname": @"Bloggs"
+                                   }
+                           };
+    NSString *result = [@"Hello {planet}! Welcome {person.firstname} {person.lastname}." stringByReplacingKeyPathBindingsWithDictionaryValues:dict];
+    XCTAssertEqualObjects(result, @"Hello World! Welcome Joe Bloggs.");
+}
+
+- (void)testThreeLevels {
+    NSDictionary *dict = @{
+                           @"planet": @"World",
+                           @"person": @{
+                                   @"firstname": @"Joe",
+                                   @"lastname": @"Bloggs",
+                                   @"job": @{
+                                           @"employer": @"Wayne Enterprises",
+                                           @"position": @"Research"
+                                           },
+                                   @"children": @[
+                                           @"Jane Bloggs",
+                                           @"Matilda Bloggs",
+                                           @"Jimmy Bloggs"
+                                           ]
+                                   }
+                           };
+    NSString *result = [@"Hello {planet}! Welcome {person.firstname} {person.lastname}. I hope you enjoy your job at {person.job.employer}." stringByReplacingKeyPathBindingsWithDictionaryValues:dict];
+    XCTAssertEqualObjects(result, @"Hello World! Welcome Joe Bloggs. I hope you enjoy your job at Wayne Enterprises.");
+
+    // FIXME Array support isn't there yet, switch these tests to test "equal" and then fix them to add it!
+    NSString *resultArr = [@"Hello {planet}! Welcome {person.firstname} {person.lastname}. I hope you enjoy your job at {person.job.employer}. You have {person.children.count} children." stringByReplacingKeyPathBindingsWithDictionaryValues:dict];
+    XCTAssertNotEqualObjects(resultArr, @"Hello World! Welcome Joe Bloggs. I hope you enjoy your job at Wayne Enterprises. You have 3 children, first of which is {person.children[0].");
+
+    NSString *resultArr2 = [@"Hello {planet}! Welcome {person.firstname} {person.lastname}. I hope you enjoy your job at {person.job.employer}. You have {person.children.count} children." stringByReplacingKeyPathBindingsWithDictionaryValues:dict];
+    XCTAssertNotEqualObjects(resultArr2, @"Hello World! Welcome Joe Bloggs. I hope you enjoy your job at Wayne Enterprises. You have 3 children, first of which is Jane Bloggs.");
+}
+
+
+- (void)testPerformanceLongStringEmptyDict {
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
+        NSString *longString = @"next_url=alarm.htm&motion_armed={params.adminparams.alarm_motion_armed}&input_armed={params.adminparams.alarm_input_armed}&motion_sensitivity={params.adminparams.alarm_motion_sensitivity}&iolinkage={params.adminparams.alarm_iolinkage}&mail={params.adminparams.alarm_mail}&upload_interval={params.adminparams.alarm_upload_interval}&ioin_level={params.adminparams.alarm_ioin_level}&ioout_level={params.adminparams.alarm_ioout_level}&schedule_enable={params.adminparams.alarm_schedule_enable}&schedule_sun_0={params.adminparams.alarm_schedule_sun_0}&schedule_sun_1={params.adminparams.alarm_schedule_sun_1}&schedule_sun_2={params.adminparams.alarm_schedule_sun_2}&schedule_mon_0={params.adminparams.alarm_schedule_mon_0}&schedule_mon_1={params.adminparams.alarm_schedule_mon_1}&schedule_mon_2={params.adminparams.alarm_schedule_mon_2}&schedule_tue_0={params.adminparams.alarm_schedule_tue_0}&schedule_tue_1={params.adminparams.alarm_schedule_tue_1}&schedule_tue_2={params.adminparams.alarm_schedule_tue_2}&schedule_wed_0={params.adminparams.alarm_schedule_wed_0}&schedule_wed_1={params.adminparams.alarm_schedule_wed_1}&schedule_wed_2={params.adminparams.alarm_schedule_wed_2}&schedule_thu_0={params.adminparams.alarm_schedule_thu_0}&schedule_thu_1={params.adminparams.alarm_schedule_thu_1}&schedule_thu_2={params.adminparams.alarm_schedule_thu_2}&schedule_fri_0={params.adminparams.alarm_schedule_fri_0}&schedule_fri_1={params.adminparams.alarm_schedule_fri_1}&schedule_fri_2={params.adminparams.alarm_schedule_fri_2}&schedule_sat_0={params.adminparams.alarm_schedule_sat_0}&schedule_sat_1={params.adminparams.alarm_schedule_sat_1}&schedule_sat_2={params.adminparams.alarm_schedule_sat_2}&user={auth.username}&pwd={auth.password}";
+        
+        NSString *replacedString = [longString stringByReplacingKeyPathBindingsWithDictionaryValues:@{} defaultValues:@{}];
+        XCTAssertEqualObjects(replacedString, @"next_url=alarm.htm&motion_armed=&input_armed=&motion_sensitivity=&iolinkage=&mail=&upload_interval=&ioin_level=&ioout_level=&schedule_enable=&schedule_sun_0=&schedule_sun_1=&schedule_sun_2=&schedule_mon_0=&schedule_mon_1=&schedule_mon_2=&schedule_tue_0=&schedule_tue_1=&schedule_tue_2=&schedule_wed_0=&schedule_wed_1=&schedule_wed_2=&schedule_thu_0=&schedule_thu_1=&schedule_thu_2=&schedule_fri_0=&schedule_fri_1=&schedule_fri_2=&schedule_sat_0=&schedule_sat_1=&schedule_sat_2=&user=&pwd=");
+    }];
+}
+
+- (void)testPerformanceLongStringLargeDict {
+    NSData *testData = [NSData dataWithContentsOfURL:[[NSBundle bundleForClass:[self class]] URLForResource:@"LargeTestDictionary" withExtension:@"json"]];
+    XCTAssertNotNil(testData);
+    NSDictionary *largeTestDictionary = @{@"params":[NSJSONSerialization JSONObjectWithData:testData options:0 error:nil]};
+    [self measureBlock:^{
+        NSString *longString = @"next_url=alarm.htm&motion_armed={params.adminparams.alarm_motion_armed}&input_armed={params.adminparams.alarm_input_armed}&motion_sensitivity={params.adminparams.alarm_motion_sensitivity}&iolinkage={params.adminparams.alarm_iolinkage}&mail={params.adminparams.alarm_mail}&upload_interval={params.adminparams.alarm_upload_interval}&ioin_level={params.adminparams.alarm_ioin_level}&ioout_level={params.adminparams.alarm_ioout_level}&schedule_enable={params.adminparams.alarm_schedule_enable}&schedule_sun_0={params.adminparams.alarm_schedule_sun_0}&schedule_sun_1={params.adminparams.alarm_schedule_sun_1}&schedule_sun_2={params.adminparams.alarm_schedule_sun_2}&schedule_mon_0={params.adminparams.alarm_schedule_mon_0}&schedule_mon_1={params.adminparams.alarm_schedule_mon_1}&schedule_mon_2={params.adminparams.alarm_schedule_mon_2}&schedule_tue_0={params.adminparams.alarm_schedule_tue_0}&schedule_tue_1={params.adminparams.alarm_schedule_tue_1}&schedule_tue_2={params.adminparams.alarm_schedule_tue_2}&schedule_wed_0={params.adminparams.alarm_schedule_wed_0}&schedule_wed_1={params.adminparams.alarm_schedule_wed_1}&schedule_wed_2={params.adminparams.alarm_schedule_wed_2}&schedule_thu_0={params.adminparams.alarm_schedule_thu_0}&schedule_thu_1={params.adminparams.alarm_schedule_thu_1}&schedule_thu_2={params.adminparams.alarm_schedule_thu_2}&schedule_fri_0={params.adminparams.alarm_schedule_fri_0}&schedule_fri_1={params.adminparams.alarm_schedule_fri_1}&schedule_fri_2={params.adminparams.alarm_schedule_fri_2}&schedule_sat_0={params.adminparams.alarm_schedule_sat_0}&schedule_sat_1={params.adminparams.alarm_schedule_sat_1}&schedule_sat_2={params.adminparams.alarm_schedule_sat_2}&user={auth.username}&pwd={auth.password}";
+        
+        NSString *replacedString = [longString stringByReplacingKeyPathBindingsWithDictionaryValues:largeTestDictionary defaultValues:@{}];
+        XCTAssertEqualObjects(replacedString, @"next_url=alarm.htm&motion_armed=1&input_armed=0&motion_sensitivity=5&iolinkage=0&mail=1&upload_interval=0&ioin_level=0&ioout_level=0&schedule_enable=0&schedule_sun_0=0&schedule_sun_1=0&schedule_sun_2=0&schedule_mon_0=0&schedule_mon_1=0&schedule_mon_2=0&schedule_tue_0=0&schedule_tue_1=0&schedule_tue_2=0&schedule_wed_0=0&schedule_wed_1=0&schedule_wed_2=0&schedule_thu_0=0&schedule_thu_1=1152&schedule_thu_2=0&schedule_fri_0=0&schedule_fri_1=0&schedule_fri_2=0&schedule_sat_0=0&schedule_sat_1=0&schedule_sat_2=0&user=&pwd=");
     }];
 }
 
